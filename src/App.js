53461fdc087526fd6e639a3d69f378a1de0259bb
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2020-12-27 13:43:16 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 
 import './App.css';
+import { Video } from 'react-video-stream';
+const url = 'http://example.com/manifest.mpd'
+ 
+const options = {
+  requestHeader: 'Authorization',
+  requestToken: 'access_token'
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world aws</h1>
+      <div>
+      <Video
+        className='video-class'
+        controls={true}
+        autoPlay={true}
+        options={options}
+        remoteUrl={url}
+      />
+    </div>
     </div>
   );
 }
